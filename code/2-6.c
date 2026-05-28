d230f3da82cf363981997c09336e409417125aa9
Euihoon Lee <uihoonpop@gmail.com>
2026-05-28 23:42:12 +0900

    2-7

--- code/2-6.c
+++ code/2-6.c
@@ -6,10 +6,18 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL)
+        return 1;
+
     const char* target_str = argv[2];
 
+    char buffer[1024];
 
-    
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (strstr(buffer, target_str) != NULL)
+            printf("%s", buffer);
+    }
+
     fclose(fp);
+    return 0;
 }
-
